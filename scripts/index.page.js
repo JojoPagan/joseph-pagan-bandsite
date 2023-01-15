a28d449207226fef92059c644a08b3e7d12bee00
JojoPagan <jpagan@nextlevelsem.com>
2023-01-15 16:38:40 -0500

    trouble shoot sas display comments and delet not showing

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -6,10 +6,14 @@ const apiKey = "e0eea5f0-0f8c-4b54-9fc4-ff50843766d4";
 //Default comments
 function displayComments(arr) {
  
-  let commentContainer = document.querySelector(".comment__default-comment");
+  let commentContainer = document.querySelector(".comment__display-comment");
   commentContainer.innerHTML = "";
   
     for (let i = 0; i < arr.length; i++) {
+
+      let m = new Date(arr[i]["timestamp"]);
+      let dateString =
+        m.getUTCMonth() + 1 + "/" + m.getUTCDate() + "/" + m.getUTCFullYear();
       
       let displaySection = document.createElement("div");
       displaySection.classList.add("comment__display");
@@ -45,72 +49,77 @@ function displayComments(arr) {
       comment.classList.add("comment__text-content-display--comment");
       comment.innerText = arr[i]["comment"];
       CommentText.appendChild(comment);
+      
+      //delete button container
+      let deleteButtonContainer = document.createElement("div");
+      deleteButtonContainer.classList.add("comment__delete-button-container");
+      defaultContainer.appendChild(deleteButtonContainer);
+  
+      //delete
+      let deleteButton = document.createElement("button");
+      deleteButton.classList.add("comment__delete-button");
+      deleteButton.addEventListener("click", event => {
+        let varId = event.target.id;
+        deleteComment(varId);
+      });
+      deleteButton.id = arr[i]["id"];
+      deleteButton.innerText = "Remove";
+      deleteButtonContainer.appendChild(deleteButton);
     }
   }
-  displayComments(comments);
   
-
+  //use Dom APi to get access to the form in html
   const form = document.querySelector(".comment__input-container");
   
+  //attach an event listener on the form of type submit in order to create new comments
   form.addEventListener("submit", submitEvent => {
+    //prevents page form reloading upon clicking submit button
     submitEvent.preventDefault();
   
-    const newComment = {};
-    newComment.name = submitEvent.target.name.value;
-    newComment.comment = submitEvent.target.comment.value;
-   
-    let commentContainer = document.querySelector(".comment__display-comment");
+    //Post Comments
+    let newAdd = axios.post(
+      "https://project-1-api.herokuapp.com/comments?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4",
+      {
+        name: submitEvent.target.name.value,
+        comment: submitEvent.target.comment.value
+      }
+    );
   
-    let displaySection = document.createElement("div");
-    displaySection.classList.add("comment__display");
-    commentContainer.appendChild(displaySection);
+    newAdd.then(() => {
+      //Get Comments
+      getComments();
+    });
   
-    let avatarImage = document.createElement("div");
-    avatarImage.classList.add("comment__comment__avatar-section-name");
-    displaySection.appendChild(avatarImage);
-  
-    let titleSection = document.createElement("div");
-    titleSection.classList.add("comment__header");
-    displaySection.appendChild(titleSection);
-  
-    let image = document.createElement("div");
-    image.classList.add("comment__title--image-avatar");
-    avatarImage.appendChild(image);
-  
-    let name = document.createElement("h2");
-    name.classList.add("comment__title--name");
-    name.innerText = newComment.name;
-    titleSection.appendChild(name);
-  
-    let todayDate = new Date();
-    let day = todayDate.getDate();
-    let month = todayDate.getMonth() +1;
-    let year = todayDate.getFullYear();
-  
-    todayDate = month + "/" + day + "/" + year;
-  
-    let date = document.createElement("h3");
-    date.innerText = todayDate;
-    date.classList.add("comment__title--date");
-    titleSection.appendChild(date);
-  
-    
-    let CommentText = document.createElement("div");
-    CommentText.classList.add("comment__text-content-display");
-    displaySection.appendChild(CommentText);
-  
-   
-    let comment = document.createElement("p");
-    comment.classList.add("comment__text-content-display--comment");
-    comment.innerText = newComment.comment;
-    CommentText.appendChild(comment);
-  
-    
-    let top = document.querySelector(".comment__display-comment");
-    top.insertBefore(displaySection, top.childNodes[0]);
-  
-    
+    //clears my input from the entry fields
     let clearInput = document.querySelector(".comment__input-container");
     clearInput.reset();
   });
-
+  
+  // function to get comments
+  function getComments() {
+    axios
+      .get(
+        "https://project-1-api.herokuapp.com/comments?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4"
+      )
+      .then(response => {
+        displayComments(
+          response.data.sort(function(a, b) {
+            return b.timestamp - a.timestamp;
+          })
+        );
+      });
+  }
+  
+  //Get Comments
+  getComments();
+  
+  //Delete Comments
+  function deleteComment(id) {
+    axios
+      .delete(
+        `https://project-1-api.herokuapp.com/comments/${id}?api_key=e0eea5f0-0f8c-4b54-9fc4-ff50843766d4`
+      )
+      .then(response => {
+        getComments();
+      });
+  }
